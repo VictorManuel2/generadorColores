eac4375f46e1eade99e21b46fb814751380b9cb3
Victor <a3518110554@alumno.uttehuacan.edu.mx>
2022-12-04 01:19:22 -0600

    Generador de colores

---
+++ sw.js
@@ -0,0 +1,42 @@
+importScripts(
+  'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js'
+);
+
+workbox.precaching.precacheAndRoute([
+  'index.html',
+  'offline.html',
+  'generador.html',
+  'icons/offline.jpg'
+]);
+
+workbox.routing.registerRoute(
+  ({request}) => request.destination === "image",
+  new workbox.strategies.NetworkOnly()
+);
+
+/*workbox.routing.registerRoute(
+  ({request}) => request.destination === "document",
+  new workbox.strategies.CacheFirst()
+);*/
+/*workbox.routing.registerRoute(
+  ({request}) => request.destination === "document",
+  new workbox.strategies.CacheOnly()
+);*/
+  workbox.routing.registerRoute(
+  ({request}) => request.destination === "document",
+  new workbox.strategies.NetworkFirst()
+  );
+
+//Si hay una respuesta que genere error
+workbox.routing.setCatchHandler(async context=>{
+  // console.log("entro");  
+  console.log(context);
+  console.log(context.request);
+  // Return the precached offline page if a document is being request
+  if (context.request.destination === 'image'){
+    return workbox.precaching.matchPrecache('icons/offline.jpg');}
+  else if(context.request.destination === 'document'){
+      return workbox.precaching.matchPrecache('offline.html');
+  }
+  return Response.error();
+});
